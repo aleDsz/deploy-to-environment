cdb6b42172dfa4b1fee739bec6b3bcbe6f0c5165
Alexandre de Souza <alexandre@aledsz.com.br>
2021-05-23 05:12:18 -0300

    :sparkles: add github actions definition

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Github from '~/services/github'
+import core from '@actions/core'
+
+// const github = new Github('api_url', 'token')
+
+async function run() {
+  try {
+    const json = process.env['GITHUB_MESSAGE']
+    const body = JSON.parse(json)
+
+    core.debug(body)
+  } catch (error) {
+    core.setFailed(error)
+  }
+}
+
+run()
